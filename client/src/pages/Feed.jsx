15af6ad212741ccc134e59547a2b21bb7949d6be
Ayush Adhikari <ayushadhikari080@gmail.com>
2026-07-06 13:10:49 +0530

    Add authentication and loading components; implement user routing
    - Integrated @clerk/react for user authentication.
    - Updated App component to conditionally render Login or Layout based on user state.
    - Created Loading component for better user experience during data fetching.
    - Enhanced Feed and Layout components with loading states and sidebar functionality.
    - Added MenuItems and Sidebar components for navigation.
    - Implemented StoriesBar for displaying user stories.
    - Updated package.json and package-lock.json with new dependencies.

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import { dummyPostsData } from "../assets/assets";
+import StoriesBar from "../components/StoriesBar";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [feedData, setFeedData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default Feed
+  const fetchFeeds = async () => {
+    setFeedData(dummyPostsData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? 
+  <div>
+    {/* stories and post */ }
+    <div>
+      <StoriesBar />
+      <div>
+        <h1>list of stories</h1>
+      </div>
+    </div>
+
+    {/*  */}
+    
+  </div> : <Loading />;
+};
+
+export default Feed;
--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
+import Loading from "../components/Loading";
+import Sidebar from "../components/Sidebar";
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+  const user = dummyUserData;
+  const [sideBarOpen, SetsideBarOpen] = useState(false);
 
-export default Layout
+  return user ? (
+    <div className="w-full flex h-screen bg-amber-400">
+      <Sidebar sideBarOpen={sideBarOpen} SetsideBarOpen={SetsideBarOpen} />
+      <div className="flex-1 bg-slate-300">
+        <Outlet />
+      </div>
+      {sideBarOpen ? (
+        <X
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => SetsideBarOpen(false)}
+        />
+      ) : (
+        <Menu
+          className="absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden"
+          onClick={() => SetsideBarOpen(true)}
+        />
+      )}
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Layout;
