095f696e22fa3e8acf3c8da0c6e529a51c5e12d7
Ayush Adhikari <ayushadhikari080@gmail.com>
2026-07-17 17:54:15 +0530

    Add Vercel configuration files for server deployment

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,6 +1,5 @@
 import { Inngest } from "inngest";
 import User from "../models/user.js";
-import { serve } from "inngest/express";
 
 export const inngest = new Inngest({
   id: "pingup-app",
@@ -16,32 +15,38 @@ const syncUserCreation = inngest.createFunction(
     ],
   },
   async ({ event }) => {
-    const {
-      id,
-      first_name,
-      last_name,
-      email_addresses,
-      image_url,
-    } = event.data;
-
-    const email = email_addresses[0].email_address;
-
-    let username = email.split("@")[0];
-
-    const existingUser = await User.findOne({ username });
-
-    if (existingUser) {
-      username += Math.floor(Math.random() * 10000);
-    }
-
-    await User.create({
-      _id: id,
-      email,
-      full_name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
-      profile_picture: image_url,
-      username,
-    });
-  }
+    // ...
+  },
 );
 
-export const functions = [syncUserCreation];
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [
+      {
+        event: "clerk/user.updated",
+      },
+    ],
+  },
+  async ({ event }) => {
+    // ...
+  },
+);
+
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-with-clerk",
+    triggers: [
+      {
+        event: "clerk/user.deleted",
+      },
+    ],
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await User.findByIdAndDelete(id);
+  },
+);
+
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
