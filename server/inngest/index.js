df2a0d0f962421ef0b304eade1be4888253d209d
Ayush Adhikari <ayushadhikari080@gmail.com>
2026-07-24 17:26:56 +0530

    Rename connection request reminder function and enhance email reminder logic for new connection requests

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -79,7 +79,7 @@ const syncUserDeletion = inngest.createFunction(
 );
 
 //Ingest function to send Reminder when a new connection request is added
-const sendConnectionsRequestReminder = inngest.createFunction(
+const sendNewConnectionsRequestReminder = inngest.createFunction(
   {
     id: "send-new-connection-request-reminder",
     triggers: [
@@ -93,7 +93,7 @@ const sendConnectionsRequestReminder = inngest.createFunction(
 
     await step.run("send-connection-request-mail", async () => {
       const connection = await Connection.findById(connectionId).populate(
-        "from_user_id to_user_id"
+        "from_user_id to_user_id",
       );
 
       const subject = "👋 New Connection Request";
@@ -126,15 +126,51 @@ const sendConnectionsRequestReminder = inngest.createFunction(
   </p>
 </div>
 `;
-
-      // send email here
-      // await sendMail({
-      //   to: connection.to_user_id.email,
-      //   subject,
-      //   html: body,
-      // });
     });
-  }
+
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run("send-connection-request-remainder", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id",
+      );
+
+      if (connection.status === "accepted") {
+        return { message: "Already accepted." };
+      }
+
+      const subject = "👋 New Connection Request";
+
+      const body = `
+<div style="font-family: Arial, sans-serif; padding: 20px;">
+  <h2>Hi ${connection.to_user_id.full_name},</h2>
+
+  <p>
+    You have a new connection request from
+    ${connection.from_user_id.full_name} - @${connection.from_user_id.username}
+  </p>
+
+  <p>
+    Click
+    <a
+      href="${process.env.FRONTEND_URL}/connections"
+      style="color:#10b981;"
+    >
+      here
+    </a>
+    to accept or reject the request.
+  </p>
+
+  <br />
+
+  <p>
+    Thanks,<br />
+    PingUp - Stay Connected
+  </p>
+</div>
+`;
+    });
+  },
 );
 
-export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion,sendNewConnectionsRequestReminder];
